0ba0c964e42e1b3407613b67215e3cb1c6d1c160
Tobias Mädel <t.maedel@alfeld.de>
2020-01-04 14:28:03 +0100

    Add SHT21 example

---
+++ examples/SHT21test/SHT21test.ino
@@ -0,0 +1,45 @@
+/*************************************************** 
+	This is an example for the SHT21 Humidity & Temp Sensor
+
+	These sensors use I2C to communicate, 2 pins are required to  
+	interface
+ ****************************************************/
+ 
+#include <Arduino.h>
+#include <Wire.h>
+#include "SHT21.h"
+
+SHT21 sht21 = SHT21();
+
+void setup()
+{
+	Serial.begin(9600);
+
+	while (!Serial)
+		delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+	Serial.println("SHT21 test");
+	if (! sht21.begin(0x40)) { 
+		Serial.println("Couldn't find SHT21");
+		while (1) delay(1);
+	}
+}
+
+
+void loop()
+{
+	if (sht21.startMeasurement()) 
+	{
+		float t = sht21.readTemperature() / 10.0;
+		float h = sht21.readHumidity();
+		Serial.print("Temp *C = "); Serial.println(t);
+		Serial.print("Hum. % = "); Serial.println(h);
+	}
+	else
+	{
+		Serial.println("Failed to read sensor data");
+	}
+	Serial.println();
+
+	delay(1000);
+}
